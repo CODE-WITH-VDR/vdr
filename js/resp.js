b836675f57511b61048e3f8dd49a311a38c18193
CODEVDR <96605996+CODEVDR@users.noreply.github.com>
2022-02-28 23:25:58 +0530

    vdr

--- js/resp.js
+++ js/resp.js
@@ -18,7 +18,7 @@ const form = document.forms['google-sheet']
 form.addEventListener('submit', e => {
   e.preventDefault()
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => alert("Thanks for Contacting us..! We Will Contact You Soon..."))
+    .then(response => alert("Thanks for Contacting me..! I Will Contact You Soon..."))
     .catch(error => console.error('Error!', error.message))
 })
 
@@ -34,7 +34,7 @@ document.body.classList.toggle('dark-theme');
 
 const f = document.getElementById('example');
 const q = document.getElementById('query');
-const google = 'https://www.google.com/search?q=site%3A+';
+const google = 'https://www.google.com/search?q=';
 const site = '';
 
 function submitted(event) {
