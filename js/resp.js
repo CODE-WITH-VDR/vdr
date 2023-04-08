0e23c4c218f732070aa3d11b967ad9a68a933281
CODEVDR <96605996+CODEVDR@users.noreply.github.com>
2023-04-08 18:20:47 +0530

    vdr

--- js/resp.js
+++ js/resp.js
@@ -1,75 +1,109 @@
-burger=document.querySelector('.burger')
-navbar=document.querySelector('.navbar')
-navList=document.querySelector('.nav-list')
-rightNav=document.querySelector('.rightNav')
+// For Add Funcnality of Navbar
+burger = document.querySelector(".burger");
+navbar = document.querySelector(".navbar");
+navList = document.querySelector(".nav-list");
+rightNav = document.querySelector(".rightNav");
 
+burger.addEventListener("click", () => {
+  rightNav.classList.toggle("v-classresp");
+  navList.classList.toggle("v-classresp");
+  navbar.classList.toggle("h-navresp");
+});
+// Code Ends
 
-burger.addEventListener('click',()=>{
-    rightNav.classList.toggle('v-classresp');
-    navList.classList.toggle('v-classresp');
-    navbar.classList.toggle('h-navresp');
-})
+//For Storing Data In Google Form
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbwyQDZNJx4rirna1Xv_oHnjAD2gFKnAHZOXGe7X9YqudzgLCMJYAomjne1pP6UjLkVyLA/exec";
+const form = document.forms["google-sheet"];
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) =>
+      alert("Thanks for Contacting me..! I Will Contact You Soon...")
+    )
+    .catch((error) => console.error("Error!", error.message));
+});
+// Code Ends
 
-const scriptURL = 'https://script.google.com/macros/s/AKfycbwyQDZNJx4rirna1Xv_oHnjAD2gFKnAHZOXGe7X9YqudzgLCMJYAomjne1pP6UjLkVyLA/exec'
-const form = document.forms['google-sheet']
+// For Sccrolling To Top
+const btnScrollToTop = document.querySelector("#btnScrollToTop");
+btnScrollToTop.addEventListener("click", function () {
+  window.scrollTo(0, 0);
+});
+// Code Ends
 
-form.addEventListener('submit', e => {
-  e.preventDefault()
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => alert("Thanks for Contacting me..! I Will Contact You Soon..."))
-    .catch(error => console.error('Error!', error.message))
-})
-
-const btnScrollToTop=document.querySelector("#btnScrollToTop");
-btnScrollToTop.addEventListener("click",function(){
-    window.scrollTo(0,0);
-})
-
+// For Changing Themes
 $(".change").on("click", function () {
   if ($("body").hasClass("dark")) {
-      $("body").removeClass("dark")
-      $(".change").text("OFF");
+    $("body").removeClass("dark");
+    $(".change").text("OFF");
   } else {
-      $("body").addClass("dark");      
-      $(".change").text("ON");
+    $("body").addClass("dark");
+    $(".change").text("ON");
   }
-
 });
+// Code Ends
 
-const f = document.getElementById('example');
-const q = document.getElementById('query');
-const google = 'https://www.google.com/search?q=';
-const site = '';
+// For Google Searh
+const f = document.getElementById("example");
+const q = document.getElementById("query");
+const google = "https://www.google.com/search?q=";
+const site = "";
 
 function submitted(event) {
   event.preventDefault();
-  const url = google + site + '+' + q.value;
-  const win = window.open(url, '_blank');
+  const url = google + site + "+" + q.value;
+  const win = window.open(url, "_blank");
   win.focus();
 }
 
-f.addEventListener('submit', submitted);
-//Reload
+f.addEventListener("submit", submitted);
+// Code Ends
 
+//For Youtube video in support page
+var substack = document.createElement("iframe");
+var loadSubscribe = document.getElementById("load-subscribe");
+substack.src = "https://www.youtube.com/embed/JKUZ_F_65nU";
+let btn1 = document.getElementById("ytvideo");
+let ctr = 0;
+btn1.onclick = () => {
+  if (ctr % 2 == 0) {
+    loadSubscribe.appendChild(substack);
+    btn1.innerHTML = "❌Close";
+  } else {
+    loadSubscribe.removeChild(substack);
+    btn1.innerHTML = "Watch Video";
+  }
+  ctr++;
+};
+// Code Ends
 
 //DISABLE CONTEXT MENU
-/*document.addEventListener('keydown', function() {
-  if (event.keyCode == 123) {
-    return false;
-  } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) {
-    return false;
-  } else if (event.ctrlKey && event.keyCode == 85) {
-    return false;
-  }
-}, false);
+document.addEventListener(
+  "keydown",
+  function () {
+    if (event.keyCode == 123) {
+      return false;
+    } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) {
+      return false;
+    } else if (event.ctrlKey && event.keyCode == 85) {
+      return false;
+    }
+  },
+  false
+);
 
 if (document.addEventListener) {
-  document.addEventListener('contextmenu', function(e) {
-  e.preventDefault();
-  }, false);
+  document.addEventListener(
+    "contextmenu",
+    function (e) {
+      e.preventDefault();
+    },
+    false
+  );
 } else {
-  document.attachEvent('oncontextmenu', function() {
-  window.event.returnValue = false;
+  document.attachEvent("oncontextmenu", function () {
+    window.event.returnValue = false;
   });
-}*/
+}
